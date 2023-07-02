1f731293351eed1d6295f89dfae6fb9bff620b4e
Bensouka <git config --global user.email>
2023-07-02 12:41:12 -0700

    loops

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_last_digit - Prints last digit of a number
+ *
+ * @n: Integer character of input
+ *
+ * Return: returns result
+ */
+
+int print_last_digit(int n)
+{
+	int m;
+
+	if (n < 0)
+	{
+		n = -n;
+	}
+	m = n % 10;
+
+	if (m < 0)
+	{
+		m = -m;
+	}
+	_putchar(m + '0');
+	return (m);
+}
