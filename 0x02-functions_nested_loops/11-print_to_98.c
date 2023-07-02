1e8f10faa1c525f5bb531e2c3f67aaad9fb94217
Bensouka <git config --global user.email>
2023-07-02 12:55:53 -0700

    loops

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,45 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - Prnts natural numbers
+ *
+ * @n: Input
+ *
+ * Return: void
+ */
+
+void print_to_98(int n)
+{
+	int a;
+	int b;
+
+	if (n <= 98)
+	{
+		for (a = n; a <= 98; a++)
+		{
+			if (a != 98)
+			{
+				printf("%d, ", a);
+			}
+			else if (a == 98)
+			{
+				printf("%d\n", a);
+			}
+		}
+	}
+	else if (n >= 98)
+	{
+		for (b = n; b >= 98; b--)
+		{
+			if (b != 98)
+			{
+				printf("%d, ", b);
+			}
+			else if (b == 98)
+			{
+				printf("%d\n", b);
+			}
+		}
+	}
+}
